9e0329d76a24ec3d904468ea019416f88e8b5a7b
Ashik Billah <148143394+abaniksyl@users.noreply.github.com>
2026-03-08 01:47:52 +0600

    added the lesson section and collecting the data from api and visualizing it into the ui

--- script/index.js
+++ script/index.js
@@ -1,11 +1,40 @@
 const loadlessons = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all")
-        .then(res => res.json())
-        .then(json => displaylesson(json.data))
+        .then((res) => res.json())
+        .then((json) => displaylesson(json.data))
 
 };
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displayLevelWord(data.data));
+}
 
+const displayLevelWord = (words) => {
+    const wordcontaineer = document.getElementById("word-container");
+    wordcontaineer.innerHTML = "";
+
+    words.forEach(word => {
+        const card = document.createElement("div");
+        card.innerHTML = `
+       <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <p class="font-semibold">Meaning /Pronounciation</p>
+
+            <div class="text-2xl font-medium font-bangla">
+                "${word.meaning} / ${word.pronunciation}"
+            </div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+        `
+        wordcontaineer.append(card);
+    });
+};
 
 const displaylesson = (lessons) => {
     const levelcontainer = document.getElementById("level-container");
@@ -14,10 +43,11 @@ const displaylesson = (lessons) => {
     for (const lesson of lessons) {
         console.log(lesson);
         const bntdiv = document.createElement("div");
-        bntdiv.innerHTML = `<button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lessons - ${lesson.level_no}</button>`;
+        bntdiv.innerHTML = `<button onclick = loadLevelWord(${lesson.level_no}) class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lessons - ${lesson.level_no}</button>`;
 
         levelcontainer.append(bntdiv);
     }
 };
 
 loadlessons();
+
